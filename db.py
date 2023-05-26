21124d1381818a4704092c28fca1141fbfbe6435
Alex van Winkel <alexicoo@gmail.com>
2023-05-25 22:17:51 -0400

    created final table

--- db.py
+++ db.py
@@ -3,23 +3,32 @@ import sqlite3
 def init_db():
     conn = sqlite3.connect('database.db')
     print("Opened database successfully")
-
-    conn.execute('CREATE TABLE IF NOT EXISTS students (first_name TEXT, last_name TEXT, group TEXT)')
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        first_name TEXT,
+        last_name TEXT,
+        group_name TEXT
+        
+    )
+    """)
+    #UNIQUE(first_name, last_name, group_name)
     print("Table created successfully")
     conn.close()
 
 def insert_student(first_name, last_name, group):
     conn = sqlite3.connect('database.db')
-    print("Opened database successfully")
-    conn.execute("INSERT INTO students (first_name, last_name, group) VALUES (?, ?, ?)", (first_name, last_name, group))
+    #print("Opened database successfully")
+    conn.execute("INSERT INTO students (first_name, last_name, group_name) VALUES (?, ?, ?)", (first_name, last_name, group))
+    #print("Records created successfully")
     conn.commit()
-    print("Records created successfully")
     conn.close()
 
-def get_students():
+def get_students(group):
     conn = sqlite3.connect('database.db')
     print("Opened database successfully")
-    cursor = conn.execute("SELECT first_name, last_name, group from students")
+    cursor = conn.execute("SELECT first_name, last_name FROM students WHERE `group_name` = ?", (group,))
     students = []
     for row in cursor:
         students.append(row)
@@ -29,7 +38,7 @@ def get_students():
 def get_student(first_name, last_name):
     conn = sqlite3.connect('database.db')
     print("Opened database successfully")
-    cursor = conn.execute("SELECT first_name, last_name, group from students WHERE first_name = ? AND last_name = ?", (first_name, last_name))
+    cursor = conn.execute("SELECT first_name, last_name, group_name from students WHERE first_name = ? AND last_name = ?", (first_name, last_name))
     student = None
     for row in cursor:
         student = row
@@ -39,7 +48,7 @@ def get_student(first_name, last_name):
 def update_student(first_name, last_name, group):
     conn = sqlite3.connect('database.db')
     print("Opened database successfully")
-    conn.execute("UPDATE students SET group = ? WHERE first_name = ? AND last_name = ?", (group, first_name, last_name))
+    conn.execute("UPDATE students SET group_name = ? WHERE first_name = ? AND last_name = ?", (group, first_name, last_name))
     conn.commit()
     print("Records updated successfully")
     conn.close()
@@ -52,3 +61,12 @@ def delete_student(first_name, last_name):
     print("Records deleted successfully")
     conn.close()
 
+def get_all_students():
+    conn = sqlite3.connect('database.db')
+    print("Opened database successfully")
+    cursor = conn.execute("SELECT first_name, last_name, group_name from students")
+    students = []
+    for row in cursor:
+        students.append(row)
+    conn.close()
+    return students
--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,24 @@
 # Importing required modules
 from flask import Flask, request, render_template
+from jinja2 import Environment, FileSystemLoader
+
 from werkzeug.utils import secure_filename
 import os
 import pandas as pd
-from db import init_db, insert_student, get_students, get_student, update_student, delete_student
+import csv
+from db import init_db, insert_student, get_students, get_student, update_student, delete_student, get_all_students
+
 
 
 # Creating Flask object
 app = Flask(__name__)
+
+
 data = []
 
+init_db()
+
 # Configuring upload folder and allowed file extensions
-#app.config['UPLOAD_FOLDER'] = '/path/to/upload/folder'
 app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'uploads')
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'csv', 'xls'}
@@ -100,14 +107,14 @@ def groups():
         if 'file' not in request.files:
             return 'No file part', 400
         file = request.files['file']
-        newfilename = request.form.get('newfilename')
+        group_name = request.form.get('groupName')
         # If user does not select file, browser submits an empty part without filename
         if file.filename == '':
             return 'No selected file', 400
-        if not newfilename:
+        if not group_name:
             return 'No filename provided', 400
         if file and allowed_file(file.filename):
-            newfilename = secure_filename(newfilename)
+            newfilename = secure_filename(group_name)
             # Keep the extension of the original file
             extension = file.filename.rsplit('.', 1)[1].lower()
             newfilename = f"students.{extension}"
@@ -130,11 +137,19 @@ def groups():
             list_2d = new_df.values.tolist()
             new_list = []
             for student in list_2d:
-                temp = student[0]
-                if temp != 'nan':
-                    new_list.append(temp)
-                else:
-                    print("NaN !!")
+                temp1 = str(student[0])[:-3] #Arcabascio , Alessia
+                if "," in temp1:
+                    if " ," in temp1:
+                        temp1 = temp1.replace(" ,",",")
+                        print(f"fixed: {temp1}")
+
+                    temp2 = temp1.split(" ")
+                    temp3 = temp2[0] + temp2[1]
+                    new_list.append(temp3)
+                    temp4 = temp3.split(",")
+                    if temp4[1] == "" or len(temp4[1]) <2:
+                        print(f"Odd name: {temp4}")
+                    insert_student(temp4[0], temp4[1], group_name)
             
 
 
@@ -145,6 +160,66 @@ def groups():
             return 'File not allowed', 400
         
     return render_template('upload_groups.html')
+
+@app.route('/table', methods=['GET','POST'])
+def table():
+    if request.method == 'POST':
+        group_name = request.form.get('groupName')
+        unit = request.form.get('unit')
+        student_list = get_students(group_name)
+        check_list = []
+        for student in student_list:
+            name = student[0] + ", " + student[1]
+            check_list.append(name)
+        #print(f"Check list: {check_list}")
+        file_name = unit + ".csv"
+        #read csv file with name unit
+        grades_list = []
+        filepath=os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+        data = pd.read_csv(filepath)
+        new_df = data[['Student']]
+
+        # Include any column that contains the current chapter
+        for col in data.columns:
+            if unit in col:
+                new_df = pd.concat([new_df, data[col]], axis=1)
+
+        list_2d = new_df.values.tolist()
+
+        points_possible = list_2d[0]
+        points_possible = points_possible[1:]
+        total_points_possible = 0
+        for i in points_possible:
+            total_points_possible += i
+
+        new_list = []
+        print(f"checklist: {check_list}")
+        for student in list_2d:
+            #for el in student:
+            #    if str(el) == "nan":
+             #       el = 0
+            if student[0] in check_list:
+                total = 0
+                for grade in student[1:]:
+                    if grade not in [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]:
+                        grade = 0
+                    total += grade
+                try:
+                    student.append(int(total/total_points_possible*100))
+                except:
+                    student.append(0)
+                new_list.append(student)
+            else:
+                print(f"NOT IN LIST: {student[0]}")
+
+
+            #print(f"filtered data: {filtered_data}")
+
+        return render_template('output_table.html', data=new_list, unit=unit, group=group_name)
+    
+
+    return render_template('ask_table_properties.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
